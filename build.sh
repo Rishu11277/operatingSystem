e4647cc475643613e8eb2c29591235ae5ba5d6a3
Rishik Goenka <116980820+Rishu11277@users.noreply.github.com>
2025-01-24 21:58:07 +0000

    Add blah

---
+++ build.sh
@@ -0,0 +1,7 @@
+nasm -f bin boot.asm -o boot.bin
+gcc -ffreestanding -o kernel.bin -nostdlib -m32 -O2 kernel.c -e _start
+dd if=boot.bin of=disk.img bs=512 count=1 conv=notrunc
+dd if=kernel.bin of=disk.img bs=512 seek=1 conv=notrunc
+git add boot.bin kernel.bin disk.img boot.asm kernel.c
+git commit -m "Add blahblah sheep"
+
