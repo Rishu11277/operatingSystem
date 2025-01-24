a1011adad7c0a8705aad6578617bf334c05e2d60
Rishik Goenka <116980820+Rishu11277@users.noreply.github.com>
2025-01-24 21:43:02 +0000

    Added bootloader and kernel code, and created disk image

--- boot.asm
+++ boot.asm
@@ -1,1 +1,24 @@
+[BITS 16]
+[ORG 0x7C00]
 
+start:
+    mov ax, 0x07C0
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7BFF
+
+    ; Load kernel (assuming it's at sector 2)
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0
+    mov bx, 0x8000
+    int 0x13
+
+    jmp 0x8000:0
+
+TIMES 510-($-$$) db 0
+dw 0xAA55
--- kernel.c
+++ kernel.c
@@ -1,1 +1,18 @@
+void main() {
+    char *video_memory = (char *)0xb8000;
+    const char *msg = "Kernel Loaded!";
+    int i = 0;
 
+    while (msg[i] != 0) {
+        video_memory[i * 2] = msg[i];
+        video_memory[i * 2 + 1] = 0x07; // Light grey on black
+        i++;
+    }
+
+    while (1) {}
+}
+
+void _start() {
+    main();
+    while (1) {}
+}
